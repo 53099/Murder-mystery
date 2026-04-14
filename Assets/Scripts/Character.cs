1e1570389afcbed0c8104e92c08bb053f5516d4a
53099 <53099@edu.raseko.fi>
2026-04-14 11:08:47 +0300

    a

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,8 +1,17 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
+public enum Characterstate
+{
+    Idle = 0,
+    Surpised = 1,
+    Angry = 2,
+}
 
 [CreateAssetMenu(menuName ="Data/Character")]
 public class Character : ScriptableObject
 {
-
+    public string Name;
+    public List<Texture> sprites;
 }
