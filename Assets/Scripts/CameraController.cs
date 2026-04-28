f4237c0bf019364b69f9d6ce76269f71283d0b5f
53099 <53099@edu.raseko.fi>
2026-04-28 08:40:02 +0300

    camera rotation done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,27 +4,28 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] float angle, rotationTime, radius;
     float speed;
+
     [SerializeField] Transform pivot, target;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Camera cam;
+    [SerializeField] float zoomFOV = 40f;
+
     void Start()
     {
-        
+        cam.fieldOfView = zoomFOV;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 targetDir = target.position - pivot.position;
+        speed = (Mathf.PI * 2f) / rotationTime;
+        angle += speed * Time.deltaTime;
 
-        speed = (Mathf.PI * 2) / rotationTime;
-        angle = Vector3.SignedAngle(targetDir, pivot.forward, Vector3.up); //speed * Time.deltaTime;
-        angle *= Mathf.Deg2Rad;
-        float x = Mathf.Sin(-angle) * radius;
+        float x = Mathf.Sin(angle) * radius;
         float z = Mathf.Cos(angle) * radius;
 
-        transform.position = new Vector3(x, transform.position.y, z);
-        transform.rotation = Quaternion.LookRotation(transform.position - pivot.position);
+        Vector3 offset = new Vector3(x, 0f, z);
+        transform.position = pivot.position + offset;
 
+        transform.LookAt(pivot.position);
     }
 }
